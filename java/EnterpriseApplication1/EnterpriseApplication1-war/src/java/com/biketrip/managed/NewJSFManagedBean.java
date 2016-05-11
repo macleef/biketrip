096becba9686a03c4f94cf3c105eeefe2ce42e7c
macleef <macleef@gmail.com>
2016-05-11 20:47:26 +0200

    Add WEB INF
    Signed-off-by: macleef <macleef@gmail.com>

---
+++ java/EnterpriseApplication1/EnterpriseApplication1-war/src/java/com/biketrip/managed/NewJSFManagedBean.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.biketrip.managed;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+/**
+ *
+ * @author florian
+ */
+@ManagedBean
+@RequestScoped
+public class NewJSFManagedBean {
+
+    /**
+     * Creates a new instance of NewJSFManagedBean
+     */
+    public NewJSFManagedBean() {
+    }
+    
+}
